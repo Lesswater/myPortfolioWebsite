88a480118705e0f9d878b89d96ea27fb20b40c7f
Jackson Moreland <12jdm5@queensu.ca>
2017-09-19 15:32:29 -0400

    Initial release
    putting first copy of my website onto a server

---
+++ js/index.js
@@ -0,0 +1,12 @@
+(function() {
+
+
+  /*$(document).scroll(function() {
+              if ($(this).scrollTop() == 0) {
+                  $("#floatingNavbar").slideDown();
+              } else {
+                  $("#floatingNavbar").slideUp();
+              }
+
+          });*/
+})();
